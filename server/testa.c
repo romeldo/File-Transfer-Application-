f62af09cfd785f0fe23c0ab060f99032f4807733
romeldo <ronald_tsang@outlook.com>
2014-04-30 15:01:05 -0400

    FTP

---
+++ server/testa.c
@@ -0,0 +1,23 @@
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <strings.h>
+
+#define BUFLEN 256
+
+int main ()
+{
+char str[50],str1[50],buffer[256];
+int i=0; 
+	printf("Filename to open: \n");
+	scanf("%s",&str);
+	FILE *fp = fopen (str, "r");
+	for ()
+
+
+
+
+return 0;
+}
